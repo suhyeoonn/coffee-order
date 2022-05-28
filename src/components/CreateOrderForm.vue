be7a15e3bd3ac0a0578b4c2e75aa80d0d2415438
suhyeon1740 <suhyeon1740@gmail.com>
2022-05-28 16:50:55 +0900

    주문서 생성 페이지 구현

---
+++ src/components/CreateOrderForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-form @submit.prevent="onSubmit" class="q-gutter-md">
+    <q-input v-model="orderName" label="주문서 이름" />
+    <q-input v-model="description" label="설명" />
+    <q-btn label="주문서 만들기" type="submit" />
+  </q-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      orderName: '',
+      description: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      //submit
+    },
+  },
+}
+</script>
+
+<style scoped>
+button {
+  width: 100%;
+  background: #ff0080;
+  color: white;
+}
+</style>
---
+++ src/views/CreateOrder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md">
+    <h1>주문서 생성</h1>
+    <CreateOrderForm />
+  </div>
+</template>
+
+<script>
+import CreateOrderForm from '@/components/CreateOrderForm'
+
+export default {
+  name: 'Home',
+  components: {
+    CreateOrderForm,
+  },
+}
+</script>
+<style scoped lang="scss">
+h1 {
+  font-size: 3rem;
+}
+div {
+  width: 400px;
+}
+</style>
