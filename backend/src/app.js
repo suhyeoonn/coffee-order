9e306128a8e7285a2b670097f9810249db6b5432
suhyeon1740 <suhyeon1740@gmail.com>
2022-06-11 20:13:26 +0900

    db 정보 .env로 관리

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
 const app = express()
+dotenv.config()
 
 const bills = require('./routes/bills')
 const drinks = require('./routes/drinks')
